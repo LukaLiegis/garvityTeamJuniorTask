13054df030cf91246a61c8258d9414881ada9b52
Luka Liegis <luka.liegis@gmail.com>
2024-07-31 09:32:03 +0300

    add transaction costs

--- trading_simulation.py
+++ trading_simulation.py
@@ -1,7 +1,8 @@
-from typing import Tuple
+from typing import Tuple, List
 import numpy as np
 
 DAYS = 100
+HOURS_PER_DAY = 24
 NORMAL_PROFIT_MARGIN = 0.016
 INCREASED_PROFIT_MARGIN = 0.10
 COINBASE_FEE = 0.00017
@@ -10,47 +11,76 @@ LOAN_INTEREST_RATE = 0.03
 LOAN_AMOUNT = 1_000_000
 TRADE_SIZE = 500
 COINBASE_TO_BINANCE_PROBABILITY = 0.57
+BINANCE_TO_COINBASE_TRANSFER_FEE = 45
+COINBASE_TO_BINANCE_TRANSFER_FEE = 62
+USD_USDT_TRANSFER_FEE = 3.5  # Average of $3 and $4
+ASSETS_UNDER_MANAGEMENT = 10_000_000
 
-def simulate_trading(prices: np.ndarray, with_loan: bool = False) -> Tuple[float, int, float]:
-    total_profit = 0
+def simulate_trading(prices: np.ndarray, with_loan: bool = False) -> Tuple[List[float], int, float]:
+    hourly_profits = []
     trade_count = 0
     loan_cost = 0
+    binance_balance = ASSETS_UNDER_MANAGEMENT / 2
+    coinbase_balance = ASSETS_UNDER_MANAGEMENT / 2
     
     for day, daily_prices in enumerate(np.array_split(prices, DAYS)):
-        daily_profit = 0
-        daily_trades = 0
-        
         for hour, price in enumerate(daily_prices):
+            hourly_profit = 0
+            
             # Increased profit margin for 1 hour every day
-            if hour == 0:
-                profit_margin = INCREASED_PROFIT_MARGIN
-            else:
-                profit_margin = NORMAL_PROFIT_MARGIN
+            profit_margin = INCREASED_PROFIT_MARGIN if hour == 0 else NORMAL_PROFIT_MARGIN
             
             if np.random.random() < COINBASE_TO_BINANCE_PROBABILITY:
                 # Buy on Coinbase, sell on Binance
-                buy_price = price * (1 - profit_margin/2)
-                sell_price = price * (1 + profit_margin/2)
+                if coinbase_balance >= TRADE_SIZE:
+                    buy_price = price * (1 - profit_margin/2)
+                    sell_price = price * (1 + profit_margin/2)
+                    trade_size = min(TRADE_SIZE, coinbase_balance)
+                    profit = (sell_price - buy_price) * trade_size
+                    profit -= trade_size * (COINBASE_FEE + BINANCE_FEE)
+                    
+                    if profit > 0:
+                        hourly_profit += profit
+                        trade_count += 1
+                        coinbase_balance -= trade_size
+                        binance_balance += trade_size + profit
             else:
                 # Buy on Binance, sell on Coinbase
-                buy_price = price * (1 - profit_margin/2)
-                sell_price = price * (1 + profit_margin/2)
+                if binance_balance >= TRADE_SIZE:
+                    buy_price = price * (1 - profit_margin/2)
+                    sell_price = price * (1 + profit_margin/2)
+                    trade_size = min(TRADE_SIZE, binance_balance)
+                    profit = (sell_price - buy_price) * trade_size
+                    profit -= trade_size * (COINBASE_FEE + BINANCE_FEE)
+                    
+                    if profit > 0:
+                        hourly_profit += profit
+                        trade_count += 1
+                        binance_balance -= trade_size
+                        coinbase_balance += trade_size + profit
             
-            trade_size = TRADE_SIZE * (2 if with_loan else 1)  # Double trade size if loan is taken
-            profit = (sell_price - buy_price) * trade_size
-            profit -= trade_size * COINBASE_FEE  # Coinbase fee
-            profit -= trade_size * BINANCE_FEE   # Binance fee
+            # Balance transfer logic
+            total_balance = binance_balance + coinbase_balance
+            if binance_balance > total_balance * 0.55:
+                transfer_amount = binance_balance - (total_balance * 0.5)
+                transfer_fee = BINANCE_TO_COINBASE_TRANSFER_FEE + USD_USDT_TRANSFER_FEE
+                if transfer_amount > transfer_fee:
+                    binance_balance -= transfer_amount
+                    coinbase_balance += transfer_amount - transfer_fee
+                    hourly_profit -= transfer_fee
+            elif coinbase_balance > total_balance * 0.55:
+                transfer_amount = coinbase_balance - (total_balance * 0.5)
+                transfer_fee = COINBASE_TO_BINANCE_TRANSFER_FEE + USD_USDT_TRANSFER_FEE
+                if transfer_amount > transfer_fee:
+                    coinbase_balance -= transfer_amount
+                    binance_balance += transfer_amount - transfer_fee
+                    hourly_profit -= transfer_fee
             
-            if profit > 0:
-                daily_profit += profit
-                daily_trades += 1
-        
-        if with_loan:
-            daily_loan_cost = LOAN_AMOUNT * LOAN_INTEREST_RATE
-            loan_cost += daily_loan_cost
-            daily_profit -= daily_loan_cost
-        
-        total_profit += daily_profit
-        trade_count += daily_trades
+            if with_loan:
+                hourly_loan_cost = (LOAN_AMOUNT * LOAN_INTEREST_RATE) / HOURS_PER_DAY
+                loan_cost += hourly_loan_cost
+                hourly_profit -= hourly_loan_cost
+            
+            hourly_profits.append(hourly_profit)
     
-    return total_profit, trade_count, loan_cost
+    return hourly_profits, trade_count, loan_cost
